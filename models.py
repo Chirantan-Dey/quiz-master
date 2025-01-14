bf661f6d218f019bba2a2651786100739f5a1803
Chirantan Dey <chiru.d.guy@gmail.com>
2025-01-14 20:31:57 +0530

    first commit

---
+++ models.py
@@ -0,0 +1,65 @@
+from extensions import db
+from flask_security import UserMixin, RoleMixin
+from flask_security.models import fsqla_v3 as fsqla
+
+fsqla.FsModels.set_db_info(db)
+
+class UserRoles(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String, nullable = False, unique = True)
+    password = db.Column(db.String)
+    active = db.Column(db.Boolean)    
+    full_name = db.Column(db.String)    
+    qualification = db.Column(db.String)
+    dob = db.Column(db.Date)
+    fs_uniquifier = db.Column(db.String(65), unique = True, nullable = False)
+    roles = db.relationship('Role', secondary='user_roles')
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = True)
+    description = db.Column(db.String(255))
+
+class StudyResource(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    topic = db.Column(db.String(80), nullable = False)
+    creator = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content = db.Column(db.String)
+    is_approved = db.Column(db.Boolean, default = False)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    date_of_quiz = db.Column(db.Date)
+    time_duration = db.Column(db.String)
+    remarks = db.Column(db.String)
+
+class Questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    question_statement = db.Column(db.String, nullable=False)
+    option1 = db.Column(db.String, nullable=False)
+    option2 = db.Column(db.String, nullable=False)
+
+class Scores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    time_stamp_of_attempt = db.Column(db.DateTime)
+    total_scored = db.Column(db.Integer)
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
