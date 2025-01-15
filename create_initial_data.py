7630bf5ed89a43419eeea179e682a008a0fae3b3
unknown <chiru.d.guy@gmail.com>
2025-01-15 08:47:34 +0530

    i

--- create_initial_data.py
+++ create_initial_data.py
@@ -1,7 +1,7 @@
 from flask_security import SQLAlchemySessionUserDatastore
 from extensions import db
 from flask_security.utils import hash_password
-from models import StudyResource
+
 
 
 def create_data(user_datastore : SQLAlchemySessionUserDatastore):
@@ -10,18 +10,16 @@ def create_data(user_datastore : SQLAlchemySessionUserDatastore):
     # creating roles
 
     user_datastore.find_or_create_role(name='admin', description = "Administrator")
-    user_datastore.find_or_create_role(name='inst', description = "Instructor")
-    user_datastore.find_or_create_role(name='stud', description = "Student")
+    
+    user_datastore.find_or_create_role(name='user', description = "User")
 
     # creating initial data
 
     if not user_datastore.find_user(email = "admin@iitm.ac.in"):
         user_datastore.create_user(email = "admin@iitm.ac.in", password = hash_password("pass"), roles=['admin'])
-    if not user_datastore.find_user(email = "inst@iitm.ac.in"):
-        user_datastore.create_user(email = "inst@iitm.ac.in", password = hash_password("pass"), roles=['inst'])
-    if not user_datastore.find_user(email = "stud@iitm.ac.in"):
-        user_datastore.create_user(email = "stud@iitm.ac.in", password = hash_password("pass"), roles=['stud'])
+    if not user_datastore.find_user(email = "user@iitm.ac.in"):
+        user_datastore.create_user(email = "user@iitm.ac.in", password = hash_password("pass"), roles=['user'])
 
-    # create dummy study resource
+    # create dummy resource
 
     db.session.commit()
--- models.py
+++ models.py
@@ -25,12 +25,6 @@ class Role(db.Model, RoleMixin):
     name = db.Column(db.String(80), unique = True)
     description = db.Column(db.String(255))
 
-class StudyResource(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    topic = db.Column(db.String(80), nullable = False)
-    creator = db.Column(db.Integer, db.ForeignKey('user.id'))
-    content = db.Column(db.String)
-    is_approved = db.Column(db.Boolean, default = False)
 
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
