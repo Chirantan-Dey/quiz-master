244b75fce8923b5dd6e9e544a78cf09d6ebc0ed4
unknown <chiru.d.guy@gmail.com>
2025-01-20 10:15:02 +0530

    navbar update

--- static/components/Navbar.js
+++ static/components/Navbar.js
@@ -1,29 +1,35 @@
 const Navbar = {
   template: `
-        <nav class= "h3 w-auto d-flex justify-content-around ">
-            <router-link :to="getHomeLink">Home</router-link>
-            <router-link :to="getQuizLink">Quiz</router-link>
-            <router-link :to="getSummaryLink">Summary</router-link>
-            <a :href="logoutURL">Logout</a>
-        </nav>
-    `,
+    <nav class="navbar navbar-dark bg-dark h3 w-100">
+      <div class="container d-flex justify-content-between">
+        <div class="d-flex align-items-center">
+          <router-link :to="getHomeLink" class="navbar-brand px-3 mb-0">Home</router-link>
+          <span class="text-light">|</span>
+          <router-link :to="getQuizLink" class="navbar-brand px-3 mb-0">Quiz</router-link>
+          <span class="text-light">|</span>
+          <router-link :to="getSummaryLink" class="navbar-brand px-3 mb-0">Summary</router-link>
+        </div>
+        <a :href="logoutURL" class="navbar-brand px-3 mb-0">Logout</a>
+      </div>
+    </nav>
+  `,
   data() {
-      return {
-        loggedIn: false,
-        logoutURL: window.location.origin + "/logout",
-      };
+    return {
+      loggedIn: false,
+      logoutURL: window.location.origin + "/logout",
+    };
+  },
+  computed: {
+    getHomeLink() {
+      return this.$store.state.user && this.$store.state.user.roles.length === 1 && this.$store.state.user.roles[0].name === 'admin' ? '/home-admin' : '/home-user';
     },
-    computed: {
-      getHomeLink() {
-        return this.$store.state.user && this.$store.state.user.roles.length === 1 && this.$store.state.user.roles[0].name === 'admin' ? '/home-admin' : '/home-user';
-      },
-      getQuizLink() {
-        return this.$store.state.user && this.$store.state.user.roles.length === 1 && this.$store.state.user.roles[0].name === 'admin' ? '/quiz-admin' : '/quiz-user';
-      },
-      getSummaryLink() {
-          return this.$store.state.user && this.$store.state.user.roles.length === 1 && this.$store.state.user.roles[0].name === 'admin' ? '/summary-admin' : '/summary-user';
-      }
+    getQuizLink() {
+      return this.$store.state.user && this.$store.state.user.roles.length === 1 && this.$store.state.user.roles[0].name === 'admin' ? '/quiz-admin' : '/quiz-user';
+    },
+    getSummaryLink() {
+      return this.$store.state.user && this.$store.state.user.roles.length === 1 && this.$store.state.user.roles[0].name === 'admin' ? '/summary-admin' : '/summary-user';
     }
+  }
 };
 
 export default Navbar;
